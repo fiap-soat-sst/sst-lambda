46b92a5aeb9c4097da68a503f73db88529f231cb
devtomasjr <dev@tomasjr.com>
2024-09-23 21:19:56 -0300

    fix: lambda function handler

--- lambda.tf
+++ lambda.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "authorizer" {
   function_name = "my_lambda_authorizer"
   handler       = "index.handler"
   runtime       = "nodejs20.x"
-  filename      = "lambda_function.zip"
+  filename      = "index.zip"
   timeout       = 10
 
   role          = data.aws_iam_role.existing_lambda_role.arn
